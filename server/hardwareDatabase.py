26c1f60d4b9b1b62ec271ae0ec193dd55ef96c38
zwaldbusser <zwaldbusser@gmail.com>
2026-07-21 17:40:01 -0500

    Initial Hardware Queries

--- server/hardwareDatabase.py
+++ server/hardwareDatabase.py
@@ -13,27 +13,73 @@ HardwareSet = {
 # Function to create a new hardware set
 def createHardwareSet(client, hwSetName, initCapacity):
     # Create a new hardware set in the database
-    pass
+    myDB = client["haas"]
+    myCol = myDB["hardwareSets"]
+    myQuery = {'hwName': hwSetName}
+    cursor = myCol.find_one(myQuery)
+    if cursor == None:
+        myDB["hardwareSets"].insert_one({
+            'hwName': hwSetName,
+            'capacity': initCapacity,
+            'availability': initCapacity,
+        })
+    else:
+        print(f"Error: hardware set '{hwSetName}' already exists")
 
 # Function to query a hardware set by its name
+#Returns a list containing capacity and availability
 def queryHardwareSet(client, hwSetName):
     # Query and return a hardware set from the database
-    pass
+    myDB = client["haas"]
+    myCol = myDB["hardwareSets"]
+    myQuery = {'hwName': hwSetName}
+    cursor = myCol.find_one(myQuery)
+    if cursor == None:
+        print(f"Error: hardware set '{hwSetName}' not found")
+    #Extract values
+    capacity = cursor.get('capacity')
+    available = cursor.get('availability')
+    return capacity, available
 
 # Function to update the availability of a hardware set
 def updateAvailability(client, hwSetName, newAvailability):
     # Update the availability of an existing hardware set
-    pass
+    myDB = client["haas"]
+    myCol = myDB["hardwareSets"]
+    query = {'hwName': hwSetName}
+    update = { "$set": {'availability': newAvailability}}
+
+    result = myCol.update_one(query, update)
+
+    if result.matched_count == 0:
+        print(f"Error: hardware set '{hwSetName}' not found")
+    elif result.modified_count == 0:
+        print(f"Warning: '{hwSetName}' found but availability unchanged")
+    else:
+        print(f"Updated '{hwSetName}' availability to {newAvailability}")
+
 
 # Function to request space from a hardware set
 def requestSpace(client, hwSetName, amount):
     # Request a certain amount of hardware and update availability
-    pass
+    capacity, avail = queryHardwareSet(client, hwSetName)
+    newAmt = avail - amount
+    if newAmt >= 0:
+        updateAvailability(client, hwSetName, newAmt)
+        print(f"Successfully requested {amount} of {hwSetName}!")
+    else:
+        print(f"Error: Not enough hardware for set '{hwSetName}'")
+
+
 
 # Function to get all hardware set names
 def getAllHwNames(client):
     # Get and return a list of all hardware set names
-    pass
+    myDB = client["haas"]
+    myCol = myDB["hardwareSets"]
+
+    cursor = myCol.find({}, {"hwName": 1, "_id": 0})
+    return [doc["hwName"] for doc in cursor]
 
 #To test database connectivity: python hardwareDatabase.py
 if __name__ == "__main__":
