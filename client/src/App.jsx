1c4a87acea1a9207331dd6eeb54c714de02b59e9
Nathan <nathan.s.palomera@gmail.com>
2026-07-28 22:33:18 -0500

    Added a create user page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
-
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import ProjectOverview from "./pages/ProjectDashboard";
 import LandingPage from "./pages/LandingPage";
+import CreateUser from "./pages/CreateUser";
 
 function App() {
   return (
@@ -11,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<CreateUser />} />
         <Route path="/projects" element={<ProjectOverview />} />
       </Routes>
     </BrowserRouter>
